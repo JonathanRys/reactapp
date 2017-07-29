ca273168841bdeb7b030f3694c74a556e73e2620
Jonathan Rys <jonathan.rk.rys@gmail.com>
2017-07-29 18:12:48 -0400

    Test CI

--- server.js
+++ server.js
@@ -4,11 +4,10 @@ const fs = require('fs')
 
 const PORT = 3000
 
-// Map the root to the build folder
+// Map the resource folders and the root to the build folder
 app.use('/img/', express.static('img'), (req, res)=>{console.log('GET/img', req.connection.remoteAddress, "<=", req.originalUrl)})
 app.use('/css/', express.static('css'), (req, res)=>{console.log('GET/css', req.connection.remoteAddress, "<=", req.originalUrl)})
 app.use('/', express.static('build'), (req, res)=>{console.log('GET/', req.connection.remoteAddress, "<=", req.originalUrl)})
-
 
 app.listen(PORT, ()=>{
     console.log("Server runnning at: http://localhost:" + PORT)
