cc3fb61528e87f95717aad148276de1842f3ea13
Jonathan Rys <jonathan.rk.rys@gmail.com>
2017-07-29 11:30:53 -0400

    Configure npm test to run mocha

---
+++ test/testtest.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('Test to test Mocha', function() {
+  describe('This is a test', function() {
+    it('should always return true', function() {
+      assert.equal(1, 1);
+    });
+  });
+});
