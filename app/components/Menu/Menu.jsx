7183a67b321b4b16da688a466ae375ac3f72cce5
Jonathan Rys <jonathan.rk.rys@gmail.com>
2017-08-04 10:58:40 -0400

    Add button click handler for menu items

--- app/components/Menu/Menu.jsx
+++ app/components/Menu/Menu.jsx
@@ -1,13 +1,19 @@
 var React = require('react');
 var FontAwesome = require('react-fontawesome');
 
+function handleClick(e) {
+    var buttons = document.querySelectorAll('nav .nav-item')
+    buttons.forEach((elem)=>{elem.className = 'nav-item'});
+    e.target.className += ' selected';
+}
+
 var Menu = React.createClass({
     render: function() {
         var menu = [];
 
         for (var i = 0; i < this.props.menuItems.length; i++) {
             var x = this.props.menuItems[i];
-            menu.push(<a key={i} href={x.link}><li className={"nav-item" + ((i === 0)? " selected": '')}><FontAwesome name={x.icon} /> {x.label}</li></a>);
+            menu.push(<a key={i} href={x.link}><li className={"nav-item" + ((i === 0)? " selected": '')} onClick={handleClick}><FontAwesome name={x.icon} /> {x.label}</li></a>);
         }
         return (<nav>
                     <ul className="nav">
